03fba8603ce9079e7ec583be7f91489e546b98c1
kumkee <kumkee@users.noreply.github.com>
2024-04-18 16:41:16 +1200

    rm unused packages; log msg: 5 -> 8

--- DotNet8Isolated/Execute.fs
+++ DotNet8Isolated/Execute.fs
@@ -10,7 +10,7 @@ type Execute(logger: ILogger<Execute>) =
     [<Function("Execute")>]        
     member _.Run([<HttpTrigger()>]  req:HttpRequestData, executionContext:FunctionContext) =
         task {
-            logger.LogInformation($"Hello at {System.DateTime.UtcNow} from an Azure function using F# on .NET 5.")
+            logger.LogInformation($"Hello at {System.DateTime.UtcNow} from an Azure function using F# on .NET 8.")
             let response = req.CreateResponse(HttpStatusCode.OK)
             do! response.WriteStringAsync("Hello World")
             return response
